916e678075340754be9b097c88122c024cb0aa2a
Martin Mirakyan <mirakyanmartin@gmail.com>
2017-06-04 17:30:25 +0400

    added utility methods for EditCorrectText

--- app/src/main/java/com/xpn/spellnote/ui/document/edit/EditCorrectText.java
+++ app/src/main/java/com/xpn/spellnote/ui/document/edit/EditCorrectText.java
@@ -2,8 +2,14 @@ package com.xpn.spellnote.ui.document.edit;
 
 import android.content.Context;
 import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
 import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 
 public class EditCorrectText extends AppCompatEditText {
@@ -50,5 +56,61 @@ public class EditCorrectText extends AppCompatEditText {
     public void markText(int left, int right, int color) {
         // color = Color.parseColor("#D20000")
         getText().setSpan( new ForegroundColorSpan(color), left, right, 0 );
+    }
+
+    /**
+     * Removes all punctuation marks and splits by ' '
+     * @return all nonempty strings found after that operation
+     */
+    public List <String> getWords(int left, int right) {
+        String[] res = getText()
+                .subSequence(left, right)   // take only the range [left, right)
+                .toString()                 // convert to immuatable string
+                .replaceAll("[.:_,]", " ")  // remove all punctuation marks with a regexp
+                .split(" ");                // split the resulting string into words by ' '
+
+        List<String> words = new ArrayList<>(Arrays.asList(res));
+        words.removeAll(Collections.singleton(""));
+        return words;
+    }
+
+    public List <String> getAllWords() {
+        return getWords( 0, getText().length() );
+    }
+
+
+    private boolean isWordCharacter(char c) {
+        return Character.isLetter(c) || c == '-';
+    }
+
+    /**
+     * @param index any index inside a word
+     * Returns first index of the word
+     */
+    public int getWordStart(int index) {
+        Editable text = getText();
+        index = Math.min( index, text.length() - 1 );
+        index = Math.max( index, 0 );
+
+        while( index > 0 && isWordCharacter(text.charAt(index-1))) {
+            --index;
+        }
+        return index;
+    }
+
+
+    /**
+     * @param index any index inside a word
+     * Returns the first index at which text[index] is not alphabetic of a connector (i.e. '-')
+     */
+    public int getWordEnd(int index) {
+        Editable text = getText();
+        index = Math.min( index, text.length() );
+        index = Math.max( index, 0 );
+
+        while( index < text.length() && isWordCharacter(text.charAt(index))) {
+            ++index;
+        }
+        return index;
     }
 }
