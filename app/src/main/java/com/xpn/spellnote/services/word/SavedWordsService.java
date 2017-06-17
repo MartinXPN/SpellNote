b9b61d658aece3927f3408a5f0456f2d5d68657d
Martin Mirakyan <mirakyanmartin@gmail.com>
2017-06-17 18:27:03 +0400

    added functionality for updating dictionary

--- app/src/main/java/com/xpn/spellnote/services/word/SavedWordsService.java
+++ app/src/main/java/com/xpn/spellnote/services/word/SavedWordsService.java
@@ -4,10 +4,13 @@ import com.xpn.spellnote.models.WordModel;
 
 import java.util.List;
 
+import io.reactivex.Completable;
 import io.reactivex.Single;
 
 
 public interface SavedWordsService {
 
     Single<List<WordModel>> getUserDefinedWords(String locale);
+    Completable saveWords(String locale, List<WordModel> words);
+    Completable saveWord(String locale, WordModel word);
 }
--- app/src/main/java/com/xpn/spellnote/services/word/local/SavedWordsServiceImpl.java
+++ app/src/main/java/com/xpn/spellnote/services/word/local/SavedWordsServiceImpl.java
@@ -9,6 +9,7 @@ import com.xpn.spellnote.services.word.SavedWordsService;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.Completable;
 import io.reactivex.Single;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -25,18 +26,22 @@ public class SavedWordsServiceImpl implements SavedWordsService {
         this.wordMapper = wordMapper;
     }
 
+    private Realm getRealmInstance(String locale) {
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .name(locale + ".realm")
+                .build();
+
+        Timber.d("Opening database at: " + realmConfiguration.getPath());
+        Realm realm = Realm.getInstance(realmConfiguration);
+        realm.refresh();
+
+        return realm;
+    }
+
     @Override
     public Single<List<WordModel>> getUserDefinedWords(String locale) {
         return Single.defer(() -> {
-            RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
-                    .name(locale + ".realm")
-                    .build();
-
-            Timber.d("Opening database at: " + realmConfiguration.getPath());
-
-            Realm realm = Realm.getInstance(realmConfiguration);
-            realm.refresh();
-
+            Realm realm = getRealmInstance(locale);
             RealmResults<WordSchema> result = realm.where(WordSchema.class)
                     .equalTo("isUserDefined", true)
                     .findAll();
@@ -45,5 +50,30 @@ public class SavedWordsServiceImpl implements SavedWordsService {
                     .map(wordMapper::mapFrom)
                     .collect(Collectors.toCollection(ArrayList::new)));
         });
+    }
+
+
+    @Override
+    public Completable saveWords(String locale, List<WordModel> words) {
+        return Completable.defer(() -> Completable.fromAction(() -> {
+            if( words.isEmpty() )
+                return;
+
+            Realm realmInstance = getRealmInstance(locale);
+            List <WordSchema> wordSchemas = Stream.of(words)
+                    .map(wordMapper::mapTo)
+                    .collect(Collectors.toCollection(ArrayList::new));
+
+            realmInstance.executeTransaction(realm -> realm.copyToRealmOrUpdate(wordSchemas));
+        }));
+    }
+
+
+    @Override
+    public Completable saveWord(String locale, WordModel word) {
+        return Completable.defer(() -> Completable.fromAction(() -> {
+            Realm realmInstance = getRealmInstance(locale);
+            realmInstance.executeTransaction(realm -> realm.copyToRealmOrUpdate(wordMapper.mapTo(word)));
+        }));
     }
 }
