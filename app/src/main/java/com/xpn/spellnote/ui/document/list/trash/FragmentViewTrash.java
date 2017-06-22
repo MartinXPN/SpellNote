a8ac88e423ac5db39cf8e6321600007de849ef96
Martin Mirakyan <mirakyanmartin@gmail.com>
2017-06-22 19:28:50 +0400

    Empty trash option added

--- app/src/main/java/com/xpn/spellnote/ui/document/list/trash/FragmentViewTrash.java
+++ app/src/main/java/com/xpn/spellnote/ui/document/list/trash/FragmentViewTrash.java
@@ -3,8 +3,12 @@ package com.xpn.spellnote.ui.document.list.trash;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.xpn.spellnote.R;
 import com.xpn.spellnote.SpellNoteApp;
@@ -24,6 +28,24 @@ public class FragmentViewTrash extends BaseFragmentDocumentList {
         binding.list.setAdapter(adapter);
         return binding.getRoot();
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate( R.menu.menu_view_trash, menu );
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item ) {
+        int id = item.getItemId();
+
+        if( id == R.id.action_empty_trash ) {
+            Toast.makeText(getActivity(), "Empty", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public String getCategory() {
