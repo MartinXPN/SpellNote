d58d2fa0821ea617e721de97eac0727a71132879
Martin Mirakyan <mirakyanmartin@gmail.com>
2017-06-23 09:15:28 +0400

    service method for removing whole category added

--- app/src/main/java/com/xpn/spellnote/services/document/DocumentService.java
+++ app/src/main/java/com/xpn/spellnote/services/document/DocumentService.java
@@ -12,6 +12,7 @@ public interface DocumentService {
 
     Completable saveDocument(DocumentModel document );
     Completable removeDocument( DocumentModel document );
+    Completable removeDocumentCategory( String category );
     Completable moveDocument( DocumentModel document, String newCategory );
     Single<DocumentModel> getDocument( Long id );
     Single <List<DocumentModel>> getAllDocuments( String category, String orderBy, boolean ascending );
