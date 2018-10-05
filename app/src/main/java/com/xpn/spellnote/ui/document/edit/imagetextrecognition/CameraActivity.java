88fdf5f557dce80d40ada21079b5326b4324b86c
MartinXPN <mirakyanmartin@gmail.com>
2018-10-05 19:03:59 +0400

    draw detected text near the actual one

--- app/src/main/java/com/xpn/spellnote/ui/document/edit/imagetextrecognition/CameraActivity.java
+++ app/src/main/java/com/xpn/spellnote/ui/document/edit/imagetextrecognition/CameraActivity.java
@@ -1,50 +1,49 @@
 package com.xpn.spellnote.ui.document.edit.imagetextrecognition;
 
 import android.Manifest;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 import com.xpn.spellnote.R;
 import com.xpn.spellnote.databinding.ActivityCameraBinding;
 
 import java.io.IOException;
 
+import timber.log.Timber;
+
+
 public class CameraActivity extends AppCompatActivity {
 
-    ActivityCameraBinding binding;
-    CameraSource cameraSource;
+    private static final int REQUEST_CAMERA_PERMISSION_ID = 1001;
 
-    final int RequestCameraPermissionID = 1001;
+    private ActivityCameraBinding binding;
+    private CameraSource cameraSource;
+
 
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case RequestCameraPermissionID: {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                        return;
-                    }
-                    try {
-                        cameraSource.start(binding.surface.getHolder());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
+        if (requestCode == REQUEST_CAMERA_PERMISSION_ID) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                try {
+                    cameraSource.start(binding.surface.getHolder());
+                } catch (IOException e) {
+                    Timber.e(e);
                 }
             }
-            break;
         }
     }
 
@@ -55,15 +54,26 @@ public class CameraActivity extends AppCompatActivity {
 
 
         TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
-        if(!textRecognizer.isOperational()) {
-            Toast.makeText(this, "Not operational!", Toast.LENGTH_SHORT).show();
+        if (!textRecognizer.isOperational()) {
+            Timber.w("Detector dependencies are not yet available.");
+
+            // Check for low storage.  If there is low storage, the native library will not be
+            // downloaded, so detection will not become operational.
+            IntentFilter lowStorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+            boolean hasLowStorage = registerReceiver(null, lowStorageFilter) != null;
+
+            if (hasLowStorage) {
+                Toast.makeText(this, "Low storage", Toast.LENGTH_LONG).show();
+                Timber.w("Low storage - cannot start text recognizer");
+            }
             return;
         }
 
+        /// TODO calculate preview size
         cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
                 .setFacing(CameraSource.CAMERA_FACING_BACK)
                 .setAutoFocusEnabled(true)
-                .setRequestedFps(2.0f)
+                .setRequestedFps(1.0f)
                 .setRequestedPreviewSize(1280, 1024)
                 .build();
 
@@ -71,22 +81,23 @@ public class CameraActivity extends AppCompatActivity {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
                 try {
+                    // Request permission to use camera if not granted yet
                     if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
 
                         ActivityCompat.requestPermissions(CameraActivity.this,
                                 new String[]{Manifest.permission.CAMERA},
-                                RequestCameraPermissionID);
+                                REQUEST_CAMERA_PERMISSION_ID);
                         return;
                     }
+                    // otherwise start the camera
                     cameraSource.start(binding.surface.getHolder());
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    Timber.e(e);
                 }
             }
 
             @Override
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
             }
 
             @Override
@@ -96,30 +107,6 @@ public class CameraActivity extends AppCompatActivity {
         });
 
 
-        textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
-            @Override
-            public void release() {
-
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<TextBlock> detections) {
-
-                final SparseArray<TextBlock> items = detections.getDetectedItems();
-                if(items.size() != 0)
-                {
-                    binding.text.post(() -> {
-                        StringBuilder stringBuilder = new StringBuilder();
-                        for(int i =0;i<items.size();++i)
-                        {
-                            TextBlock item = items.valueAt(i);
-                            stringBuilder.append(item.getValue());
-                            stringBuilder.append("\n");
-                        }
-                        binding.text.setText(stringBuilder.toString());
-                    });
-                }
-            }
-        });
+        textRecognizer.setProcessor(new OcrDetectorProcessor(binding.overlay));
     }
 }
---
+++ app/src/main/java/com/xpn/spellnote/ui/document/edit/imagetextrecognition/OcrDetectorProcessor.java
@@ -0,0 +1,36 @@
+package com.xpn.spellnote.ui.document.edit.imagetextrecognition;
+
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+
+import timber.log.Timber;
+
+
+public class OcrDetectorProcessor implements Detector.Processor<TextBlock> {
+    private GraphicOverlay<OcrGraphic> graphicOverlay;
+
+    OcrDetectorProcessor(GraphicOverlay<OcrGraphic> ocrGraphicOverlay) {
+        graphicOverlay = ocrGraphicOverlay;
+    }
+
+    @Override
+    public void receiveDetections(Detector.Detections<TextBlock> detections) {
+        graphicOverlay.clear();
+        SparseArray<TextBlock> items = detections.getDetectedItems();
+        for (int i = 0; i < items.size(); ++i) {
+            TextBlock item = items.valueAt(i);
+            if (item != null && item.getValue() != null) {
+                Timber.d("Text detected! %s", item.getValue());
+                OcrGraphic graphic = new OcrGraphic(graphicOverlay, item);
+                graphicOverlay.add(graphic);
+            }
+        }
+    }
+
+    @Override
+    public void release() {
+        graphicOverlay.clear();
+    }
+}
