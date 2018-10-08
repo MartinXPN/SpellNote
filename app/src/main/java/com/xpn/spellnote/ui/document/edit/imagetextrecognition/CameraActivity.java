2ba525a5633c48b0fe0e7aa612d76ae92133216e
MartinXPN <mirakyanmartin@gmail.com>
2018-10-08 19:23:23 +0400

    integrate camera view library

--- app/src/main/java/com/xpn/spellnote/ui/document/edit/imagetextrecognition/CameraActivity.java
+++ app/src/main/java/com/xpn/spellnote/ui/document/edit/imagetextrecognition/CameraActivity.java
@@ -1,41 +1,67 @@
 package com.xpn.spellnote.ui.document.edit.imagetextrecognition;
 
-import android.content.Context;
-import android.content.res.AssetManager;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Pair;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Toast;
 
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.document.FirebaseVisionDocumentText;
 import com.google.firebase.ml.vision.document.FirebaseVisionDocumentTextRecognizer;
+import com.otaliastudios.cameraview.CameraListener;
+import com.otaliastudios.cameraview.CameraUtils;
 import com.xpn.spellnote.R;
 import com.xpn.spellnote.databinding.ActivityCameraBinding;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 
-public class CameraActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    private Bitmap mSelectedImage;
+public class CameraActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA_PERMISSION_ID = 1001;
+
     private ActivityCameraBinding binding;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION_ID:
+                if (permissions.length != 1 || grantResults.length != 1) {
+                    throw new RuntimeException("Error on requesting camera permission.");
+                }
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Can\'t use camera without the permission", Toast.LENGTH_SHORT).show();
+                }
+                // No need to start camera here; it is handled by onResume
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_camera);
-        binding.capture.setOnClickListener(v -> runCloudTextRecognition());
+        binding.camera.setLifecycleOwner(this);
+        binding.capture.setOnClickListener(v -> binding.camera.capturePicture());
+
+        binding.camera.addCameraListener(new CameraListener() {
+            @Override
+            public void onPictureTaken(byte[] picture) {
+                // Create a bitmap or a file...
+                // CameraUtils will read EXIF orientation for you, in a worker thread.
+                CameraUtils.decodeBitmap(picture, bitmap -> runCloudTextRecognition(bitmap));
+            }
+        });
     }
 
-    private void runCloudTextRecognition() {
+    private void runCloudTextRecognition(Bitmap mSelectedImage) {
         binding.capture.setEnabled(false);
         FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mSelectedImage);
         FirebaseVisionDocumentTextRecognizer detector = FirebaseVision.getInstance()
@@ -73,73 +99,5 @@ public class CameraActivity extends AppCompatActivity implements AdapterView.OnI
                 }
             }
         }
-    }
-
-    // Gets the targeted width / height.
-    private Pair<Integer, Integer> getTargetedWidthHeight() {
-        int targetWidth = binding.imageView.getWidth();
-        int targetHeight = binding.imageView.getHeight();
-        return new Pair<>(targetWidth, targetHeight);
-    }
-
-    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
-        binding.graphicOverlay.clear();
-        switch (position) {
-            case 0:
-                mSelectedImage = getBitmapFromAsset(this, "Please_walk_on_the_grass.jpg");
-
-                break;
-            case 1:
-                // Whatever you want to happen when the second item gets selected
-                mSelectedImage = getBitmapFromAsset(this, "non-latin.jpg");
-                break;
-            case 2:
-                // Whatever you want to happen when the thrid item gets selected
-                mSelectedImage = getBitmapFromAsset(this, "nl2.jpg");
-                break;
-        }
-        if (mSelectedImage != null) {
-            // Get the dimensions of the View
-            Pair<Integer, Integer> targetedSize = getTargetedWidthHeight();
-
-            int targetWidth = targetedSize.first;
-            int maxHeight = targetedSize.second;
-
-            // Determine how much to scale down the image
-            float scaleFactor =
-                    Math.max(
-                            (float) mSelectedImage.getWidth() / (float) targetWidth,
-                            (float) mSelectedImage.getHeight() / (float) maxHeight);
-
-            Bitmap resizedBitmap =
-                    Bitmap.createScaledBitmap(
-                            mSelectedImage,
-                            (int) (mSelectedImage.getWidth() / scaleFactor),
-                            (int) (mSelectedImage.getHeight() / scaleFactor),
-                            true);
-
-            binding.imageView.setImageBitmap(resizedBitmap);
-            mSelectedImage = resizedBitmap;
-        }
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Do nothing
-    }
-
-    public static Bitmap getBitmapFromAsset(Context context, String filePath) {
-        AssetManager assetManager = context.getAssets();
-
-        InputStream is;
-        Bitmap bitmap = null;
-        try {
-            is = assetManager.open(filePath);
-            bitmap = BitmapFactory.decodeStream(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return bitmap;
     }
 }
